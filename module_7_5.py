7f9edcfe10ddd77601e81a3075cdd3a4db36a746
lomaster064 <64ilya64@gmail.com>
2024-11-03 07:47:04 -0800

    Update module_7_5.py

--- module_7_5.py
+++ module_7_5.py
@@ -10,8 +10,8 @@ for root, dirs, files in os.walk(directory):
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(file_time))
 
         # parent directory
-        # parent_dir = os.path.dirname(file_path)
-        parent_dir = os.path.abspath(file_path)
+        parent_dir = os.path.dirname(file_path)
+        # parent_dir = os.path.abspath(file_path)
 
         stat_info = os.stat(file) # ругается на невозможность найти определённый файл (.gitignore), в общем работает, поставленную задачу выполняет (если убрать папку с гит-файлами)
         file_size = stat_info.st_size
